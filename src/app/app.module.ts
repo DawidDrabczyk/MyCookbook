66ade4a24aa18a51005c02ac0da28aa422b39054
ddrabczyk <ddrabczyk@gabos.pl>
2022-12-22 21:04:25 +0100

    Add file picker without logic and apply reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
@@ -22,6 +22,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
--- src/app/recipes/recipe-create/recipe-create.component.ts
+++ src/app/recipes/recipe-create/recipe-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Recipe } from 'src/app/models/recipe.model';
 import { RecipeService } from 'src/app/services/recipe.service';
@@ -13,21 +13,27 @@ export class RecipeCreateComponent implements OnInit {
   newRecipe: string = 'Brak dodanych przepisów!';
   private mode: string = 'create';
   private recipeId;
+  form: FormGroup;
   recipe;
   isLoading: boolean = false;
 
   constructor(public recipeService: RecipeService, public activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(null, { validators: [Validators.required] }),
+      content: new FormControl(null, { validators: [Validators.required] }),
+    });
     this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('recipeId')) {
         this.mode = 'edit';
         this.recipeId = paramMap.get('recipeId');
         this.isLoading = true;
-        this.recipeService.getRecipe(this.recipeId).subscribe(recipeData => {
+        this.recipeService.getRecipe(this.recipeId).subscribe((recipeData) => {
           this.isLoading = false;
-          this.recipe = { id: recipeData._id, title: recipeData.title, content: recipeData.content}
-        })
+          this.recipe = { id: recipeData._id, title: recipeData.title, content: recipeData.content };
+          this.form.setValue({ title: this.recipe.title, content: this.recipe.content });
+        });
       } else {
         this.mode = 'create';
         this.recipeId = null;
@@ -35,17 +41,17 @@ export class RecipeCreateComponent implements OnInit {
     });
   }
 
-  onSaveRecipe(form: NgForm) {
-    if (form.invalid) {
+  onSaveRecipe() {
+    if (this.form.invalid) {
       return;
     }
     this.isLoading = true;
     if (this.mode === 'create') {
-      this.recipeService.addRecipe(form.value.title, form.value.content);
+      this.recipeService.addRecipe(this.form.value.title, this.form.value.content);
     } else {
-      this.recipeService.updateRecipe(this.recipeId, form.value.title, form.value.content);
+      this.recipeService.updateRecipe(this.recipeId, this.form.value.title, this.form.value.content);
     }
 
-    form.resetForm();
+    this.form.reset();
   }
 }
