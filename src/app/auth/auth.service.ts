e84240aa2809fc16b4e8260fdce38df738186341
ddrabczyk <ddrabczyk@gabos.pl>
2022-12-28 21:15:17 +0100

    Add first basic authentication settings

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -11,7 +11,15 @@ export class AuthService {
 
   createNewUser(email: string, password: string){
     const authData: AuthData = {email: email, password: password}
-    this.http.post('http://localhost:3000/api/auth/signup/', authData)
+    this.http.post('http://localhost:3000/api/auth/signup', authData)
+      .subscribe(result => {
+        console.log(result);
+      })
+  }
+
+  login(email: string, password: string){
+    const authData: AuthData = {email: email, password: password}
+    this.http.post('http://localhost:3000/api/auth/login', authData)
       .subscribe(result => {
         console.log(result);
       })
